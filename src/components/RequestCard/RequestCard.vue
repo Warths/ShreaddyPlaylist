4771884d28c77f9c7ed99423d42595ea0febd4fb
Warths <pro.warths@gmail.com>
2021-12-23 23:57:55 +0100

    Added Userlevel logic to card admin panel

--- src/components/RequestCard/RequestCard.vue
+++ src/components/RequestCard/RequestCard.vue
@@ -4,7 +4,7 @@
         <div class="card-body d-flex flex-column pt-1 pb-2">
             <!-- QUICK BUTTONS -->
             <transition name="fade">
-                <div v-if="showPanel" class="d-flex flex-wrap gap-1">
+                <div v-if="adminPanelVisible" class="d-flex flex-wrap gap-1">
                         <div class="pt-1"><button type="button" class="btn btn-primary bg-purple border-0 p-1 p-sm-2">Play</button></div>
                         <div class="pt-1"><button type="button" class="btn btn-primary bg-purple border-0 p-1 p-sm-2">Freeze</button></div>
                         <!-- Custom Dropdown button-->
@@ -38,11 +38,15 @@ import BaseRow from "./BaseRow.vue"
 
 export default {
     computed: {
+        adminPanelVisible() {
+            console.log(this.userLevel)
+            return this.showPanel && this.userLevel == 2
+        },
         tagWrapperClass() {
-            return this.showPanel ? "py-1" : "py-2 pb-1"
+            return this.adminPanelVisible ? "py-1" : "py-2 pb-1"
         }
     },
-    props: ["song", "showPanel"],
+    props: ["song", "showPanel", "userLevel"],
     components: {TagList, RequesterRow, BaseRow}
 }
 </script>
