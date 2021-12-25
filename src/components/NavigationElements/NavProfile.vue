1741eb3790336880233bb256f9101fe27acade3e
Warths <pro.warths@gmail.com>
2021-12-25 12:11:40 +0100

    Added transition to profile info apparition

--- src/components/NavigationElements/NavProfile.vue
+++ src/components/NavigationElements/NavProfile.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="align-items-center d-flex">
-    <a class="nav-link p-0">{{displayName}}</a>
-    <img v-if="userData != null" :src="avatar" class="ms-2 rounded sized-1 d-inline-block align-text-top" alt="Votre photo de profil"/>
+    <transition name="fade">
+    <div v-if="ready" class="align-items-center d-flex">
+        <a class="nav-link p-0">{{displayName}}</a>
+        <img v-if="userData != null" :src="avatar" class="ms-2 rounded sized-1 d-inline-block align-text-top" alt="Votre photo de profil"/>
     </div>
+    </transition>
 </template>
 
 <script>
 export default {
     computed: {
+      ready() {
+        return this.userData != null
+      },
       displayName() {
         if (this.userData != null) {
             return this.userData.display_name
