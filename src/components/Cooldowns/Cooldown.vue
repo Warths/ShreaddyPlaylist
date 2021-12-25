fa577e8464418f58dc9b62c0a84b30d1a5de4d0a
Warths <pro.warths@gmail.com>
2021-12-25 22:01:14 +0100

    Added center image and margin

--- src/components/Cooldowns/Cooldown.vue
+++ src/components/Cooldowns/Cooldown.vue
@@ -1,8 +1,8 @@
 <template>
-<svg :height="size" :width="size">
+<svg :height="size" :width="size" class="me-2">
   <circle :cx="size/2" :cy="size/2" :r="size/2 - (strokeWidth/2 + 1)" :style="{...dashOffset, ...stroke}"/>
   <circle class="print" :cx="size/2" :cy="size/2" :r="size/2 - (strokeWidth/2 + 1)" :style="stroke"/>
-  <!--<image href="mdn_logo_only_color.png" :height="size" :width="size"/>-->
+  <image :href="icon" :height="size" :width="size"/>
 </svg>
 </template>
 
@@ -36,6 +36,10 @@ export default {
         strokeDashArray: {
             type: Number,
             default: 82
+        },
+        icon: {
+            type: String,
+            default: ""
         }
     }
 }
@@ -44,19 +48,36 @@ export default {
 <style scoped>
 svg {
     position:relative;
+    transition: all 0.3s ease-out;
 }
 
 circle {
   position:absolute;
   fill: transparent;
   stroke-dasharray: 82; /* Magic value, STFU and don't ask */
-  stroke: rgb(80, 80, 80);
+  stroke: rgb(146, 146, 146);
   transform: translateY(100%) rotate(-90deg)
 
 }
 
 .print {
   opacity: 0.2;
+}
+
+image {
+    transform-origin: center;
+    transform: scale(0.6);
+    opacity: 0.7;
+    filter: grayscale(100%);
+}
+
+.unavailable {
+    opacity: 0.3;
+}
+
+svg:hover {
+    transform-origin: center;
+    transform: scale(1.2);
 }
 </style>
 
