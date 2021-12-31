7003b41dd022a1e7d6918c27c8bc504816c3e5f9
Warths <pro.warths@gmail.com>
2021-12-31 14:20:10 +0100

    added text-overflow protection on baserows

--- src/components/RequestCard/BaseRow.vue
+++ src/components/RequestCard/BaseRow.vue
@@ -11,6 +11,9 @@ export default {
 <style scoped>
 .card-text {
     transition: color 1s;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow:hidden;
 }
 .dark-theme .card-text {
     color: white;
