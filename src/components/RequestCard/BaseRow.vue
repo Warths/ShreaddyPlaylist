fc11fc224cf94ed99bcb2fbc7778907eb2f30e1b
Warths <pro.warths@gmail.com>
2021-12-31 17:44:34 +0100

    Added Fade transition for changing BaseRow text

--- src/components/RequestCard/BaseRow.vue
+++ src/components/RequestCard/BaseRow.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="card-text">{{ text }}</div>
+    <transition-group tag="div" name="fadable">
+        <div class="card-text fadable-item" :key="text">{{ text }}</div>
+    </transition-group>
 </template>
 
 <script>
@@ -9,13 +11,33 @@ export default {
 </script>
 
 <style scoped>
-.card-text {
+@keyframes FadeIn {
+    from {opacity:0}
+    50% {opacity:0}
+    to {opacity: 1}
+}
+
+.fadable-item {
     transition: color 1s;
     text-overflow: ellipsis;
     white-space: nowrap;
     overflow:hidden;
 }
-.dark-theme .card-text {
+
+.fadable-enter-from, .fadable-leave-to {
+    opacity:0;
+}
+
+.fadable-enter-active {
+    animation: FadeIn 1s;
+}
+
+.fadable-leave-active {
+    animation: FadeIn 1s reverse;
+    position: absolute
+}
+
+.dark-theme .fadable-item {
     color: white;
 }
 </style>
