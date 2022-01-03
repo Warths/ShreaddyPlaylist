31cd7a030e92d7728fec20f1bc20151c9707d1e5
Warths <pro.warths@gmail.com>
2022-01-03 14:12:35 +0100

    Added contrast utilitary methods

--- src/mixins/ContrastUtils.vue
+++ src/mixins/ContrastUtils.vue
@@ -16,6 +16,19 @@ export default {
             var brightest = Math.max(lum1, lum2);
             var darkest = Math.min(lum1, lum2);
             return (brightest + 0.05) / (darkest + 0.05);
+        },
+        RGBfromHex(hex) {
+            if (hex.startsWith("#")) {
+                hex = hex.replace("#", "")
+            }
+            let r = parseInt(hex.slice(0,2), 16)
+            let g = parseInt(hex.slice(2,4), 16)
+            let b = parseInt(hex.slice(4,6), 16)
+            console.log([r,g,b])
+            return [r,g,b]
+        },
+        contrastHex(hex1, hex2) {
+            return this.contrast(this.RGBfromHex(hex1), this.RGBfromHex(hex2))
         }
     }
 } 
