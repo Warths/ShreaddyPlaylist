b77ad7fac19cbc64cde5947152e2d63dd16f69d6
Warths <pro.warths@gmail.com>
2022-01-03 11:13:43 +0100

    File refractor, added duration method

---
+++ src/mixins/TimeUtils.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    methods: {
+        ellapsedTimeFrom(basis) {
+            return Date.now() - basis
+        },
+        durationToText(duration, omitSeconds=True) {
+            // converting duration from milliseconds to seconds
+            let seconds = Math.floor(duration / 1000)
+            let minutes = Math.floor(seconds / 60) % 60
+            let hours = Math.floor(seconds / 60 / 60)
+            let text = ""
+            text += `${hours}h ${minutes}min`
+            if (!omitSeconds) {
+                text += ` ${seconds % 60}s`
+            }
+            return text
+        },
+        durationFromEllapsedText(basis, omitSeconds=true) {
+            return this.durationToText(this.ellapsedTimeFrom(basis), omitSeconds)
+        }
+    }
+}
+</script>
