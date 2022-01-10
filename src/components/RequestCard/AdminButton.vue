98ea155b0745b43a936bcbbbdad202defd5d5b75
Warths <pro.warths@gmail.com>
2022-01-10 10:38:48 +0100

    Added formatting logic

--- src/components/RequestCard/AdminButton.vue
+++ src/components/RequestCard/AdminButton.vue
@@ -1,21 +1,32 @@
 <template>
     <div class="pt-1">
-        <button type="button" @click="fire()" class="btn btn-primary bg-purple border-0 p-1 px-2" :class="dangerClass">{{ text }}</button>
+        <button type="button" @click="$emit('sendCommand', formatCommand())" class="btn btn-primary bg-purple border-0 p-1 px-2" :class="dangerClass">{{ text }}</button>
     </div>
 </template>
 
 <script>
 export default {
     methods: {
-        fire() {
-            console.log("prout")
-        },  
+        formatCommand() {
+            let cmd = this.command
+            for (let key in this.song) {
+                if (typeof(this.song[key] == "object")) {
+                    for (let subkey in this.song[key]) {
+                        cmd = cmd.replace(`%${key}.${subkey}%`, this.song[key][subkey])
+                    }
+                } else {
+                    cmd = cmd.replace(`%${key}%`, this.song[key])
+                }
+            }
+            return cmd
+        }
     },
     computed: {
         dangerClass() {
             return this.danger ? "btn-danger" : ""
         }
     },
-    props: ["songId", "text", "command", "danger"]
+    emits: ["sendCommand"],
+    props: ["song", "text", "command", "danger"]
 }
 </script>
--- src/components/RequestCard/AdminButtonDropDown.vue
+++ src/components/RequestCard/AdminButtonDropDown.vue
@@ -2,7 +2,7 @@
     <div class="btn-group pt-1">
         <button type="button" class="btn btn-primary dropdown-toggle bg-purple border-0 p-1 px-2" data-bs-toggle="dropdown" aria-expanded="false">{{ text }}</button>
         <ul class="dropdown-menu">
-            <li v-for="(choice, i) in choices" :key="i"><a @click="fire(choice)" class="dropdown-item" href="#">{{ choice }}</a></li>
+            <li v-for="(choice, i) in choices" :key="i"><a @click="$emit('sendCommand', formatCommand(choice[0]))" class="dropdown-item" href="#">{{ choice[1] }}</a></li>
         </ul>
     </div>
 </template>
@@ -10,8 +10,20 @@
 <script>
 export default {
     methods: {
-        fire(option) {
-            console.log("prout", option)
+        formatCommand(choice) {
+            let cmd = this.command
+            cmd = cmd.replace("%choice%", choice)
+            for (let key in this.song) {
+                if (typeof(this.song[key] == "object")) {
+                    for (let subkey in this.song[key]) {
+                        cmd = cmd.replace(`%${key}.${subkey}%`, this.song[key][subkey])
+                    }
+                } else {
+                    cmd = cmd.replace(`%${key}%`, this.song[key])
+                }
+            }
+            console.log(cmd)
+            return cmd
         }
     },
     computed: {
@@ -19,6 +31,7 @@ export default {
             return this.danger ? "btn-danger" : ""
         }
     },
-    props: ["songId", "text", "command", "danger", "choices"]
+    emits: ["sendCommand"],
+    props: ["song", "text", "command", "danger", "choices"]
 }
 </script>
