83f925ad0f9171794e1029132f68fe99f15b9282
Warths <pro.warths@gmail.com>
2022-01-10 08:52:38 +0100

    Added new types of buttons

---
+++ src/components/RequestCard/AdminButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="pt-1">
+        <button type="button" @click="fire()" class="btn btn-primary bg-purple border-0 p-1 p-sm-2" :class="dangerClass">{{ text }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        fire() {
+            console.log("prout")
+        },  
+    },
+    computed: {
+        dangerClass() {
+            return this.danger ? "btn-danger" : ""
+        }
+    },
+    props: ["songId", "text", "command", "danger"]
+}
+</script>
---
+++ src/components/RequestCard/AdminButtonDropDown.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="btn-group pt-1">
+        <button type="button" class="btn btn-primary dropdown-toggle bg-purple border-0 p-1 p-sm-2" data-bs-toggle="dropdown" aria-expanded="false">{{ text }}</button>
+        <ul class="dropdown-menu">
+            <li v-for="(choice, i) in choices" :key="i"><a @click="fire(choice)" class="dropdown-item" href="#">{{ choice }}</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        fire(option) {
+            console.log("prout", option)
+        }
+    },
+    computed: {
+        dangerClass() {
+            return this.danger ? "btn-danger" : ""
+        }
+    },
+    props: ["songId", "text", "command", "danger", "choices"]
+}
+</script>
