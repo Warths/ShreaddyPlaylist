f78b18705e14191c2de3a325a873350f9cea075b
Warths <pro.warths@gmail.com>
2022-01-11 15:22:04 +0100

    Removed old references of login view

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
   }
 ]
 
