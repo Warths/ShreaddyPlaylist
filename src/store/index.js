94664a2f38c8ddb1c72901a9c4d02c26481918b5
Warths <pro.warths@gmail.com>
2022-01-19 15:27:39 +0100

    Added pubsub to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,25 @@
 import { createStore, mapState } from "vuex"
+import PubSubClient from '../vendors/PubSubClient.js'
 
 export default createStore({
     state() {
-        return {count: 0}
+        return {
+            pubsub: new PubSubClient("wss://pubsub.warths.fr/", 240, false)
+        }
+    },
+    mutations: {},
+    actions: {
+        subscribe(context, payload) {
+            console.log("Store Subscribe")
+            this.state.pubsub.subscribe(payload)
+        },
+        publish(context, payload) {
+            console.log("Store Publish")
+            this.state.pubsub.publish(payload)
+        },
+        addHandler(context, payload) {
+            console.log("Store addHandler")
+            this.state.pubsub.addHandler(...payload)
+        },
     }
 })
