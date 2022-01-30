c3234ad94a16c49e5317e42fec9279cbbff06f40
Warths <pro.warths@gmail.com>
2022-01-30 11:29:50 +0100

    minor style change

--- src/components/RequestCard/RequestCard.vue
+++ src/components/RequestCard/RequestCard.vue
@@ -15,17 +15,17 @@
                         />
                 </div>
             </transition>
-            <div class="d-flex gap-1">
+            <div class="d-flex gap-2">
                 <div v-if="this.option('moreInfo')" class="d-flex flex-column w-50 py-1 overflow-hidden">
-                    <tag-list class="ms-auto overflow-hidden" :tags="song.admin.tags"/>
+                    <tag-list class="ms-auto overflow-hidden " :tags="song.admin.tags"/>
                     <div class="w-100 justify-content-between d-flex flex-column">
-                        <div class="d-flex flex-column mw-100">
+                        <div class="d-flex flex-column mw-100 pe-1">
                             <base-row class="fs-animated opacity-0 fs-4" :text="song.admin.title"/>
                             <base-row class="text-end fs-animated fs-5" :text="song.admin.artist"/>
                         </div>
                         <div v-if="!option('foldPlaylist')" class="position-relative clip-to-right w-100 guitars">
                             <div class="d-flex">
-                                <requester-row class="ms-auto" :requester="song.admin.requester"/>
+                                <requester-row class="pe-1" :requester="song.admin.requester"/>
                             </div>
                         </div>
                     </div>
